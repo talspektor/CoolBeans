cb0cd06e0d83d30045eaf5abbbd7bcb3e0f91beb
Tal Spektor <tal.s@zemingo.com>
2023-11-11 13:13:12 +0200

    Initial Commit

---
+++ CoolBeans/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CoolBeans
+//
+//  Created by Tal talspektor on 11/11/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CoolBeans/CoolBeansApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoolBeansApp.swift
+//  CoolBeans
+//
+//  Created by Tal talspektor on 11/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct CoolBeansApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
